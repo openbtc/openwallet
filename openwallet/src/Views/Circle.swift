17821c875cfdb1c5f6c67ed0e3e974d1a211ee74
openbtc <openbtc@outlook.com>
2018-01-12 17:40:01 +0800

    Add views to StartViewController

---
+++ openwallet/src/Views/Circle.swift
@@ -0,0 +1,33 @@
+//
+//  Circle.swift
+//  openwallet
+//
+//  Created by Adrian Corscadden on 2016-10-24.
+//  Copyright © 2016 openwallet LLC. All rights reserved.
+//
+
+import UIKit
+
+class Circle: UIView {
+
+    let color: UIColor
+
+    static let defaultSize: CGFloat = 64.0
+
+    init(color: UIColor) {
+        self.color = color
+        super.init(frame: CGRect())
+        backgroundColor = .clear
+    }
+
+    override func draw(_ rect: CGRect) {
+        guard let ctx = UIGraphicsGetCurrentContext() else { return }
+        ctx.addEllipse(in: rect)
+        ctx.setFillColor(color.cgColor)
+        ctx.fillPath()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
