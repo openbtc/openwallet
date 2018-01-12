3ae4343b6a5238a3f67c8e59145691031a62c28c
openbtc <openbtc@outlook.com>
2018-01-12 12:56:01 +0800

    Add root view controller, delete main storyboard

---
+++ openwallet/src/ViewControllers/ReceiveViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ReceiveViewController.swift
+//  openwallet
+//
+//  Created by Adrian Corscadden on 2016-10-21.
+//  Copyright © 2016 openwallet LLC. All rights reserved.
+//
+
+import UIKit
+
+class ReceiveViewController: UIViewController {
+    
+}
---
+++ openwallet/src/ViewControllers/SendViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SendViewController.swift
+//  openwallet
+//
+//  Created by Adrian Corscadden on 2016-10-21.
+//  Copyright © 2016 openwallet LLC. All rights reserved.
+//
+
+import UIKit
+
+class SendViewController: UIViewController {
+    
+}
