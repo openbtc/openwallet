4966e0710d8460e20f499ca2a9c1e0968fcd40f3
openbtc <openbtc@outlook.com>
2018-01-13 11:09:01 +0800

    Add paper phrase state type

---
+++ openwallet/src/ViewControllers/PaperPhraseViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PaperPhraseViewController.swift
+//  openwallet
+//
+//  Created by Adrian Corscadden on 2016-10-25.
+//  Copyright © 2016 openwallet LLC. All rights reserved.
+//
+
+import UIKit
+
+class PaperPhraseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        view.backgroundColor = .white
+    }
+
+}
