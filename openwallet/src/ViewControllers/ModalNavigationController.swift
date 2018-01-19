9f698936dc20c823eda2d23f453bcd5abc7de378
openbtc <openbtc@outlook.com>
2018-01-19 11:43:01 +0800

    Switch to UIViewController based status bar style

---
+++ openwallet/src/ViewControllers/ModalNavigationController.swift
@@ -0,0 +1,15 @@
+//
+//  ModalNavigationController.swift
+//  openwallet
+//
+//  Created by Adrian Corscadden on 2016-11-23.
+//  Copyright © 2016 openwallet LLC. All rights reserved.
+//
+
+import UIKit
+
+class ModalNavigationController: UINavigationController {
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .default
+    }
+}
