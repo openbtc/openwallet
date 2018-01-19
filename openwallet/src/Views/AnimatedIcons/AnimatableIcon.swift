700c13885cfe2e0b608e7fa505a61cd05f713493
openbtc <openbtc@outlook.com>
2018-01-19 09:12:01 +0800

    Add animated icon and gradient asset to wallet creation

---
+++ openwallet/src/Views/AnimatedIcons/AnimatableIcon.swift
@@ -0,0 +1,13 @@
+//
+//  AnimatableIcon.swift
+//  openwallet
+//
+//  Created by Adrian Corscadden on 2016-11-22.
+//  Copyright © 2016 openwallet LLC. All rights reserved.
+//
+
+import UIKit
+
+protocol AnimatableIcon {
+    func animate()
+}
