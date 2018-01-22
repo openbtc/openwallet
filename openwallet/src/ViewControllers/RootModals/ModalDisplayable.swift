bc42e34f89210f26dd13d43f698b2b890461315f
openbtc <openbtc@outlook.com>
2018-01-22 10:57:02 +0800

    Add menu modal and modal container

---
+++ openwallet/src/ViewControllers/RootModals/ModalDisplayable.swift
@@ -0,0 +1,13 @@
+//
+//  ModalDisplayable.swift
+//  openwallet
+//
+//  Created by Adrian Corscadden on 2016-12-01.
+//  Copyright © 2016 openwallet LLC. All rights reserved.
+//
+
+import Foundation
+
+protocol ModalDisplayable {
+    var title: String { get }
+}
