35f9c4aeb860278ba38af4d6666eca0c4fe27acc
openbtc <openbtc@outlook.com>
2018-01-27 21:36:02 +0800

    Add initial scan view controller

---
+++ openwallet/src/ScanViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CameraViewController.swift
+//  openwallet
+//
+//  Created by Adrian Corscadden on 2016-12-12.
+//  Copyright © 2016 openwallet LLC. All rights reserved.
+//
+
+import Foundation
