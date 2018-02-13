6bd292e0b63bd7b004afd95301bc73c9f805e37c
openbtc <openbtc@outlook.com>
2018-02-13 14:18:02 +0800

    Add update pin functionality

---
+++ openwallet/src/ViewControllers/SecurityCenter/SecurityCenterViewController.swift
@@ -0,0 +1,140 @@
+//
+//  SecurityCenterViewController.swift
+//  openwallet
+//
+//  Created by Adrian Corscadden on 2017-02-14.
+//  Copyright © 2017 openwallet LLC. All rights reserved.
+//
+
+import UIKit
+
+class SecurityCenterHeader : UIView, GradientDrawable {
+    override func draw(_ rect: CGRect) {
+        drawGradient(rect)
+    }
+}
+
+private let headerHeight: CGFloat = 222.0
+
+class SecurityCenterViewController : UIViewController {
+
+    var didTapPin: (() -> Void)?
+    var didTapTouchId: (() -> Void)?
+    var didTapPaperKey: (() -> Void)?
+
+    override func viewDidLoad() {
+        setupSubviewProperties()
+        addSubviews()
+        addConstraints()
+    }
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+
+    private func setupSubviewProperties() {
+        view.backgroundColor = .white
+        header.closeCallback = {
+            self.dismiss(animated: true, completion: nil)
+        }
+        scrollView.alwaysBounceVertical = true
+        scrollView.panGestureRecognizer.delaysTouchesBegan = false
+        scrollView.delegate = self
+        info.text = S.SecurityCenter.info
+        info.numberOfLines = 0
+        info.lineBreakMode = .byWordWrapping
+
+        pinCell.isCheckHighlighted = true
+        paperKeyCell.isCheckHighlighted = true
+
+        pinCell.addTarget(self, action: #selector(SecurityCenterViewController.pinTapped), for: .touchUpInside)
+        touchIdCell.addTarget(self, action: #selector(SecurityCenterViewController.touchIdTapped), for: .touchUpInside)
+        paperKeyCell.addTarget(self, action: #selector(SecurityCenterViewController.paperKeyTapped), for: .touchUpInside)
+    }
+
+    private func addSubviews() {
+        view.addSubview(scrollView)
+        scrollView.addSubview(headerBackground)
+        headerBackground.addSubview(header)
+        headerBackground.addSubview(shield)
+        scrollView.addSubview(pinCell)
+        scrollView.addSubview(touchIdCell)
+        scrollView.addSubview(paperKeyCell)
+        scrollView.addSubview(info)
+    }
+
+    private func addConstraints() {
+        scrollView.constrain([
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),    scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor) ])
+        headerBackground.constrain([
+            headerBackground.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            headerBackground.topAnchor.constraint(equalTo: view.topAnchor),
+            headerBackground.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            headerBackground.widthAnchor.constraint(equalTo: view.widthAnchor) ])
+        headerBackgroundHeight = headerBackground.heightAnchor.constraint(equalToConstant: headerHeight)
+        headerBackground.constrain([headerBackgroundHeight])
+        header.constrain([
+            header.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            header.topAnchor.constraint(equalTo: headerBackground.topAnchor, constant: 20.0),
+            header.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            header.heightAnchor.constraint(equalToConstant: C.Sizes.headerHeight)])
+        shield.constrain([
+            shield.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            shield.centerYAnchor.constraint(equalTo: headerBackground.centerYAnchor, constant: C.padding[3]) ])
+        info.constrain([
+            info.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: C.padding[2]),
+            info.topAnchor.constraint(equalTo: headerBackground.bottomAnchor, constant: C.padding[2]),
+            info.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -C.padding[4]) ])
+        scrollView.addSubview(separator)
+        separator.constrain([
+            separator.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: C.padding[2]),
+            separator.topAnchor.constraint(equalTo: info.bottomAnchor, constant: C.padding[2]),
+            separator.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -C.padding[2]),
+            separator.heightAnchor.constraint(equalToConstant: 1.0) ])
+        pinCell.constrain([
+            pinCell.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            pinCell.topAnchor.constraint(equalTo: separator.bottomAnchor),
+            pinCell.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor) ])
+        touchIdCell.constrain([
+            touchIdCell.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            touchIdCell.topAnchor.constraint(equalTo: pinCell.bottomAnchor),
+            touchIdCell.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor) ])
+        paperKeyCell.constrain([
+            paperKeyCell.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            paperKeyCell.topAnchor.constraint(equalTo: touchIdCell.bottomAnchor),
+            paperKeyCell.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            paperKeyCell.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -C.padding[2]) ])
+    }
+
+    @objc private func pinTapped() {
+        didTapPin?()
+    }
+
+    @objc private func touchIdTapped() {
+        didTapTouchId?()
+    }
+
+    @objc private func paperKeyTapped() {
+        didTapPaperKey?()
+    }
+
+    fileprivate var headerBackgroundHeight: NSLayoutConstraint?
+    private let headerBackground = SecurityCenterHeader()
+    private let header = ModalHeaderView(title: S.SecurityCenter.title, isFaqHidden: false, style: .light)
+    private let shield = UIImageView(image: #imageLiteral(resourceName: "shield"))
+    private let scrollView = UIScrollView()
+    private let info = UILabel(font: .customBody(size: 16.0))
+    private let pinCell = SecurityCenterCell(title: S.SecurityCenter.Cells.pinTitle, descriptionText: S.SecurityCenter.Cells.pinDescription)
+    private let touchIdCell = SecurityCenterCell(title: S.SecurityCenter.Cells.touchIdTitle, descriptionText: S.SecurityCenter.Cells.touchIdDescription)
+    private let paperKeyCell = SecurityCenterCell(title: S.SecurityCenter.Cells.paperKeyTitle, descriptionText: S.SecurityCenter.Cells.paperKeyDescription)
+    private let separator = UIView(color: .secondaryShadow)
+}
+
+extension SecurityCenterViewController : UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let yOffset = scrollView.contentOffset.y
+        headerBackgroundHeight?.constant = headerHeight - yOffset
+    }
+}
---
+++ openwallet/src/ViewControllers/SecurityCenter/UpdatePinViewController.swift
@@ -0,0 +1,164 @@
+//
+//  UpdatePinViewController.swift
+//  openwallet
+//
+//  Created by Adrian Corscadden on 2017-02-16.
+//  Copyright © 2017 openwallet LLC. All rights reserved.
+//
+
+import UIKit
+
+class UpdatePinViewController : UIViewController {
+
+    //MARK: - Public
+    init(store: Store, walletManager: WalletManager) {
+        self.store = store
+        self.walletManager = walletManager
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    //MARK: - Private
+    private let header = UILabel.wrapping(font: .customBold(size: 26.0), color: .darkText)
+    private let instruction = UILabel.wrapping(font: .customBody(size: 14.0), color: .darkText)
+    private let caption = UILabel.wrapping(font: .customBody(size: 13.0), color: .secondaryGrayText)
+    private let pinView = PinView(style: .create)
+    private let pinPad = PinPadViewController(style: .white, keyboardType: .pinPad)
+    private let store: Store
+    private let walletManager: WalletManager
+    private var step: Step = .current {
+        didSet {
+            switch step {
+            case .current:
+                instruction.text = S.UpdatePin.enterCurrent
+            case .new:
+                instruction.text = S.UpdatePin.enterNew
+                caption.text = S.UpdatePin.caption
+            case .confirmNew:
+                instruction.text = S.UpdatePin.reEnterNew
+            }
+        }
+    }
+    private var currentPin: String?
+    private var newPin: String?
+
+    private enum Step {
+        case current
+        case new
+        case confirmNew
+    }
+
+    override func viewDidLoad() {
+        addSubviews()
+        addConstraints()
+        setData()
+    }
+
+    private func addSubviews() {
+        view.addSubview(header)
+        view.addSubview(instruction)
+        view.addSubview(caption)
+        view.addSubview(pinView)
+    }
+
+    private func addConstraints() {
+        header.constrain([
+            header.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: C.padding[2]),
+            header.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: C.padding[2]),
+            header.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -C.padding[2]) ])
+        instruction.constrain([
+            instruction.leadingAnchor.constraint(equalTo: header.leadingAnchor),
+            instruction.topAnchor.constraint(equalTo: header.bottomAnchor, constant: C.padding[2]),
+            instruction.trailingAnchor.constraint(equalTo: header.trailingAnchor) ])
+        pinView.constrain([
+            pinView.topAnchor.constraint(equalTo: instruction.bottomAnchor, constant: C.padding[6]),
+            pinView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            pinView.widthAnchor.constraint(equalToConstant: pinView.width),
+            pinView.heightAnchor.constraint(equalToConstant: pinView.itemSize) ])
+
+        addChildViewController(pinPad, layout: {
+            pinPad.view.constrainBottomCorners(sidePadding: 0.0, bottomPadding: 0.0)
+            pinPad.view.constrain([pinPad.view.heightAnchor.constraint(equalToConstant: pinPad.height) ])
+        })
+
+        caption.constrain([
+            caption.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: C.padding[2]),
+            caption.bottomAnchor.constraint(equalTo: pinPad.view.topAnchor, constant: -C.padding[2]),
+            caption.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -C.padding[2]) ])
+    }
+
+    private func setData() {
+        view.backgroundColor = .white
+
+        header.text = S.UpdatePin.title
+        instruction.text = S.UpdatePin.enterCurrent
+
+        pinPad.ouputDidUpdate = { text in
+            switch self.step {
+            case .current:
+                self.didUpdateForCurrent(pin: text)
+            case .new :
+                self.didUpdateForNew(pin: text)
+            case .confirmNew:
+                self.didUpdateForConfirmNew(pin: text)
+            }
+        }
+    }
+
+    private func didUpdateForCurrent(pin: String) {
+        pinView.fill(pin.utf8.count)
+        if pin.utf8.count == 6 {
+            if walletManager.authenticate(pin: pin) {
+                pinPad.clear()
+                step = .new
+                currentPin = pin
+                DispatchQueue.main.asyncAfter(deadline: .now() + pinView.shakeDuration) { [weak self] in
+                    self?.pinView.fill(0)
+                }
+            } else {
+                DispatchQueue.main.asyncAfter(deadline: .now() + pinView.shakeDuration) { [weak self] in
+                    self?.pinView.fill(0)
+                }
+                pinView.shake()
+                pinPad.clear()
+            }
+        }
+    }
+
+    private func didUpdateForNew(pin: String) {
+        pinView.fill(pin.utf8.count)
+        if pin.utf8.count == 6 {
+            pinPad.clear()
+            step = .confirmNew
+            newPin = pin
+            DispatchQueue.main.asyncAfter(deadline: .now() + pinView.shakeDuration) { [weak self] in
+                self?.pinView.fill(0)
+            }
+        }
+    }
+
+    private func didUpdateForConfirmNew(pin: String) {
+        guard let currentPin = currentPin else { return }
+        guard let newPin = newPin else { return }
+        pinView.fill(pin.utf8.count)
+        if pin.utf8.count == 6 {
+            if pin == newPin {
+                let result = walletManager.forceSetPin(newPin: pin, seedPhrase: walletManager.seedPhrase(pin: currentPin))
+                let message = result ? "Success" : "Failed"
+                let alert = UIAlertController(title: "Set pin", message: message, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .destructive, handler: nil))
+                present(alert, animated: true, completion: nil)
+
+            } else {
+                DispatchQueue.main.asyncAfter(deadline: .now() + pinView.shakeDuration) { [weak self] in
+                    self?.pinView.fill(0)
+                }
+                pinView.shake()
+                pinPad.clear()
+            }
+        }
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
