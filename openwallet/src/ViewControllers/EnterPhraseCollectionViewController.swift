ee788683a8e5c7ccee7289f2fd6d448d91bb058b
openbtc <openbtc@outlook.com>
2018-02-18 01:05:02 +0800

    Add recover wallet flow

---
+++ openwallet/src/ViewControllers/EnterPhraseCollectionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  EnterPhraseCollectionViewController.swift
+//  openwallet
+//
+//  Created by Adrian Corscadden on 2017-02-23.
+//  Copyright © 2017 openwallet LLC. All rights reserved.
+//
+
+import UIKit
+
+class EnterPhraseCollectionViewController : UICollectionViewController {
+    
+}
