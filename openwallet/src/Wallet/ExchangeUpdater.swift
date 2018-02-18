fd91eb01358b3ac0c9aa0fd5adda5d4062e69d15
openbtc <openbtc@outlook.com>
2018-02-18 19:35:02 +0800

    Rename ExchangeManager to ExchangeUpdater

---
+++ openwallet/src/Wallet/ExchangeUpdater.swift
@@ -0,0 +1,30 @@
+//
+//  ExchangeUpdater.swift
+//  openwallet
+//
+//  Created by Adrian Corscadden on 2017-01-27.
+//  Copyright © 2017 openwallet LLC. All rights reserved.
+//
+
+import Foundation
+
+class ExchangeUpdater {
+
+    //MARK: - Public
+    init(store: Store, apiClient: BRAPIClient) {
+        self.store = store
+        self.apiClient = apiClient
+    }
+
+    func refresh() {
+        apiClient.exchangeRates { rates, error in
+            guard let currencyCode = Locale.current.currencyCode else { return }
+            guard let currentRate = rates.first( where: { $0.code == currencyCode }) else { return }
+            self.store.perform(action: ExchangeRates.setRate(currentRate))
+        }
+    }
+
+    //MARK: - Private
+    let store: Store
+    let apiClient: BRAPIClient
+}
