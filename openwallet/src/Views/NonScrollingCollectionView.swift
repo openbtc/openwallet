645dc898f2c923bfbf4953e3d52748e4e4b75ba4
openbtc <openbtc@outlook.com>
2018-02-18 13:37:02 +0800

    Update enter phrase view sizes and disable auto collection view scrolling

---
+++ openwallet/src/Views/NonScrollingCollectionView.swift
@@ -0,0 +1,18 @@
+//
+//  NonScrollingCollectionView.swift
+//  openwallet
+//
+//  Created by Adrian Corscadden on 2017-02-28.
+//  Copyright © 2017 openwallet LLC. All rights reserved.
+//
+
+import UIKit
+
+//This class disables all scrolling. This is desired
+//when we don't want the scrollView to scroll to the active
+//textField
+class NonScrollingCollectionView : UICollectionView {
+    override func setContentOffset(_ contentOffset: CGPoint, animated: Bool) {
+        //noop
+    }
+}
